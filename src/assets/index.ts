29b318f2e1629769a15e0fdb494b39a25fc06759
Akerele Adebayo <jubrilake@gmail.com>
2025-01-25 20:59:46 +0100

    initial commit

---
+++ src/assets/index.ts
@@ -0,0 +1,9 @@
+import logo from "./chowlogo.svg";
+import cloud from "./cloud.svg";
+import dbcloud from "./dbcloud.svg";
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import stroke from "./stroke.png";
+import twitter from "./twitter.svg";
+
+export { logo, cloud, dbcloud, facebook, instagram, stroke, twitter };
